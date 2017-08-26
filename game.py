d79437fe387cfeee45be00ca429ac9f46945f594
Alex Beard <bearda@purdue.edu>
2017-08-26 15:14:38 +0530

    first commit

---
+++ game.py
@@ -0,0 +1,11 @@
+#This is a game. Layers of nueral nets affect the core node, which players
+#Try to set above or below a given threshold. The connections are unkown
+
+from neural import Node
+
+
+
+
+
+if __name__ == "__main__":
+    print ("Off to the races!")
