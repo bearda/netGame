b6c96d89b7b95d0fa2ade9b9e734261eed8d65ec
Alex Beard <bearda@purdue.edu>
2017-08-31 22:22:18 +0530

    piece stubs

--- NodeMenu.py
+++ NodeMenu.py
@@ -18,16 +18,24 @@ class NodeMenu(Toplevel):
         Button(self.buttonFrame, text="Randomize", command=self.randomize).grid(row=0, column=0)
         Button(self.buttonFrame, text="Back", command=self.destroy).grid(row=0, column=1)
         self.buttonFrame.grid()
+
+        self.pieceFrame = Frame(self)
+        Button(self.pieceFrame, text="Upper", command=lambda: self.setPiece(1)).grid(row=0, column=0)
+        Button(self.pieceFrame, text="Downer", command=lambda: self.setPiece(-1)).grid(row=0, column=1)
+        self.pieceFrame.grid()
         print ("off to the sub races!")
 
     def populateDisplayFrame(self, master):
         self.displayVar = StringVar()
-        self.updateString()
+        self.pieceVar = StringVar()
 
+        self.updateStrings()
+        
         Label(self.displayFrame, text="Node Input Weights:").grid(padx=10,pady=(10,0))
         Label(self.displayFrame, textvariable=self.displayVar).grid()
+        Label(self.displayFrame, textvariable=self.pieceVar).grid(padx=10,pady=(10,0))
 
-    def updateString(self):
+    def updateStrings(self):
         #we have a tought string to make.
         #it is made of a unknown number of digits, seperated by spaces
         newString = ""
@@ -35,9 +43,16 @@ class NodeMenu(Toplevel):
             newString += "%d " % weight
             self.displayVar.set(newString)
 
+        pieceString = "Piece Number: " + str(self.node.piece)
+        self.pieceVar.set(pieceString)
+
     def randomize(self):
         #first assign each weight a random number between -2 and 2
         self.node.weights = [randint(-2,2) for i in range(len(self.node.weights))]
         self.updateString()
+
+    def setPiece(self, value):
+        self.node.piece = value
+        self.updateStrings()
 
 
